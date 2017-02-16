3e0d237b32618967d8a1ab3e664dbb3c629695f0
cypress <romka_gorev@mail.ru>
2017-02-16 14:16:20 +0600

    delete -poll, add comments in webpack.config

--- webapp/webpack.config.babel.js
+++ webapp/webpack.config.babel.js
@@ -13,23 +13,26 @@ import FailPlugin from 'webpack-fail-plugin';
 import tslintConfig from './tslint.json';
 import packageJSON from './package.json'
 
-const env = process.env.NODE_ENV;  // development | test | production
+// const env = process.env.NODE_ENV;  // development | test | production
 
-// const isDevelopmentEnv = env === 'development';
-const isDevelopmentEnv = true;
-const isTestEnv = env === 'test';
-const isProductionEnv = env === 'production';
+// const isDevelopmentEnv = true;
+// const isTestEnv = env === 'test';
+// const isProductionEnv = env === 'production';
 
-assert(isDevelopmentEnv || isTestEnv || isProductionEnv, "Invalid NODE_ENV");
+// assert(isDevelopmentEnv || isTestEnv || isProductionEnv, "Invalid NODE_ENV");
 
-// const isDevServer =
-//   path.basename(require.main.filename) === 'webpack-dev-server.js';
-
-const isDevServer = true;
+// const isDevServer = true;
 
 const devServerPort = 24285;
 
 const commonConfig = {
+  entry: _.compact([
+    `webpack-dev-server/client?http://0.0.0.0:${devServerPort}`,
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    './src/index',
+  ]),
+
   resolve: {
     extensions: [
       '',
@@ -39,11 +42,6 @@ const commonConfig = {
     ],
 
     root: path.resolve('./src'),
-
-    // alias: {
-    //   Hello: path.resolve('./src/slices'),
-    //   'react/lib/ReactMount': 'react-dom/lib/ReactMount'
-    // },
   },
 
   module: {
@@ -65,89 +63,20 @@ const commonConfig = {
       {
         test: /\.jpe?g$|\.gif$|\.png$|\.ico|\.svg$|\.woff2?$|\.eot$|\.ttf$|\.wav$/,
         loader: 'file'
-      }
-    ]
-  },
-
-  plugins: _.compact([
-    // prevent from loading all locales for `moment`
-    isDevelopmentEnv &&
-    new ContextReplacementPlugin(/moment\/locale$/, /en-gb/),
-
-    // isDevelopmentEnv && new PrefetchPlugin('ice'),
-    // isDevelopmentEnv && new PrefetchPlugin('material-ui'),
-
-    new ProvidePlugin({
-      React: 'react'
-    }),
-
-    new DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env),
-      'APP_VERSION': JSON.stringify(packageJSON.version.replace(/\.0$/, '')),
-    }),
-
-    // make webpack exit with error on build errors
-    !isDevServer && FailPlugin,
-  ]),
-
-  tslint: {
-    configuration: tslintConfig,
-
-    emitErrors: true,
-  },
-
-  watchOptions: {
-    poll: true
-  },
-};
-
-
-const createConfig = (config) =>
-  _.mergeWith({}, commonConfig, config, (objValue, srcValue) => {
-    if (_.isArray(objValue)) {
-      return objValue.concat(srcValue);
-    }
-  });
-
-
-const buildConfig = createConfig({
-  entry: _.compact([
-    isDevServer && `webpack-dev-server/client?http://0.0.0.0:${devServerPort}`,
-    isDevServer && 'webpack/hot/only-dev-server',
-    'babel-polyfill',
-    './src/index',
-  ]),
-
-  module: {
-    loaders: [
+      },
       {
         test: /\.tsx?$/,
         loaders: _.compact([
-          isDevServer && 'react-hot',
+          'react-hot',
           `awesome-typescript-loader?${JSON.stringify({
+            silent: true,
             forkChecker: true,
             useBabel: true,
-            useCache: isDevServer,
+            useCache: false, // isDevServer
           })}`,
         ]),
       },
-    ],
-  },
-
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  },
-
-  output: {
-    path: path.resolve('build'),
-    publicPath: "/",
-    filename: 'main.js',
-    sourceMapFilename: '[file].map',
-    chunkFilename: "[hash]/js/[id].js",
-    hotUpdateMainFilename: "[hash]/update.json",
-    hotUpdateChunkFilename: "[hash]/js/[id].update.js"
+    ]
   },
 
   plugins: _.compact([
@@ -155,24 +84,77 @@ const buildConfig = createConfig({
       template: 'src/index.html'
     }),
 
-    isDevServer && new ForkCheckerPlugin(),
+    // isDevServer &&
+    new ForkCheckerPlugin(),
 
-    isProductionEnv && new optimize.OccurrenceOrderPlugin(true),
-    isProductionEnv && new optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
+    // isProductionEnv &&
+    // new optimize.OccurrenceOrderPlugin(true),
+    // isProductionEnv &&
+    // new optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
+
+    // prevent from loading all locales for `moment`
+    // isDevelopmentEnv &&
+    // new ContextReplacementPlugin(/moment\/locale$/, /en-gb/),
+
+    // isDevelopmentEnv && new PrefetchPlugin('ice'),
+    // isDevelopmentEnv && new PrefetchPlugin('material-ui'),
+
+    // new PrefetchPlugin('ice'),
+    // new PrefetchPlugin('material-ui'),
+
+    new ProvidePlugin({
+      React: 'react'
     }),
+
+    // new DefinePlugin({
+    //   'process.env.NODE_ENV': JSON.stringify(env),
+    //   'APP_VERSION': JSON.stringify(packageJSON.version.replace(/\.0$/, '')),
+    // }),
+
+    // make webpack exit with error on build errors
+    // !isDevServer &&
+    // FailPlugin,
   ]),
 
-  devtool: isProductionEnv ? 'source-map' : 'eval',
+  tslint: {
+    configuration: tslintConfig,
+    emitErrors: false,
+  },
 
   devServer: {
     contentBase: 'build',
     host: '0.0.0.0',
     port: devServerPort,
     historyApiFallback: true,
-  }
-});
+  },
 
-export default buildConfig;
+  // node: {
+  //   fs: 'empty',
+  //   net: 'empty',
+  //   tls: 'empty'
+  // },
+
+  // devtool: isProductionEnv ? 'source-map' : 'eval',
+  // devtool: 'eval',
+
+  // output: {
+  //   path: path.resolve('build'),
+  //   publicPath: "/",
+  //   filename: 'main.js',
+  //   sourceMapFilename: '[file].map',
+  //   chunkFilename: "[hash]/js/[id].js",
+  //   hotUpdateMainFilename: "[hash]/update.json",
+  //   hotUpdateChunkFilename: "[hash]/js/[id].update.js"
+  // },
+
+  // watchOptions: {
+  //   ignored: /node_modules/
+  //   // poll: true
+  // },
+};
+
+export default commonConfig;
