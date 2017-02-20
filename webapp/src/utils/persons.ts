bd427ddecf94323a733300c1cd0f845604466ff7
cypress <romka_gorev@mail.ru>
2017-02-21 01:34:26 +0600

    fixes, grls ratings added

--- webapp/src/utils/persons.ts
+++ webapp/src/utils/persons.ts
@@ -3,7 +3,18 @@ import config from '../configs';
 
 export function comparePersonsByParam(paramName) {
     return function (person1, person2) {
-        if (Number(person1.data[paramName].replace(',', '.')) < Number(person2.data[paramName].replace(',', '.'))) {
+        if (person1.data[paramName] == null) {
+            return 1;
+        }
+
+        if (person2.data[paramName] == null) {
+            return -1;
+        }
+
+        let num1 = (typeof person1.data[paramName] == 'string') ? Number(person1.data[paramName].replace(',', '.')) : Number(person1.data[paramName]);
+        let num2 = (typeof person2.data[paramName] == 'string') ? Number(person2.data[paramName].replace(',', '.')) : Number(person2.data[paramName]);
+
+        if (num1 < num2) {
             console.log(1);
             return 1;
         }
@@ -15,14 +26,22 @@ export function comparePersonsByParam(paramName) {
 }
 
 export function compute(person) {
+    console.log('COMPUTE PESRON', person.name);
     let tree = config.parametersTree;
     Object.keys(tree).map((key) => {
         if (Array.isArray(tree[key])) {
             let sum = 0;
             tree[key].map((paramName) => {
-                sum += Number(person.data[paramName]);
+                let num = (typeof person.data[paramName] == 'string') ? Number(person.data[paramName].replace(',', '.')) : Number(person.data[paramName]);
+                sum += num;
             });
-            person.data[key] = (sum / tree[key].length);
+
+            console.log('SUM', key, sum);
+            console.log('LENGHT', key, tree[key].length);
+
+            let res = (sum / tree[key].length);
+            console.log('RES FOR KEY', key, res);
+            person.data[key] = res;
         }
         else {
             console.error('IS NOT ARRAY');
